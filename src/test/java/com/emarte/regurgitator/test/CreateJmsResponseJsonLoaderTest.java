2fc40b82f33aceaa83766fd1d3f4352d543fabb7
talmeym <mtalmey@gmail.com>
2017-10-09 17:48:38 +0100

    tidied indentation

--- src/test/java/com/emarte/regurgitator/test/CreateJmsResponseJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/CreateJmsResponseJsonLoaderTest.java
@@ -11,17 +11,17 @@ import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
 
 public class CreateJmsResponseJsonLoaderTest extends JsonLoaderTest {
 
-	public CreateJmsResponseJsonLoaderTest() {
-		super(new CreateJmsResponseJsonLoader());
-	}
+    public CreateJmsResponseJsonLoaderTest() {
+        super(new CreateJmsResponseJsonLoader());
+    }
 
-	@Test
-	public void testThis() throws Exception {
-		assertExpectation("classpath:/CreateJmsResponse.json", "com.emarte.regurgitator.extensions.mq.CreateJmsResponse:[com.emarte.regurgitator.core.CreateResponse:['create-jsm-response-1',com.emarte.regurgitator.core.ValueSource:[null,'value'],null],'message-id','type','destination','correlation','4','expiration','priority','true','reply-to','123456789']");
-	}
+    @Test
+    public void testThis() throws Exception {
+        assertExpectation("classpath:/CreateJmsResponse.json", "com.emarte.regurgitator.extensions.mq.CreateJmsResponse:[com.emarte.regurgitator.core.CreateResponse:['create-jsm-response-1',com.emarte.regurgitator.core.ValueSource:[null,'value'],null],'message-id','type','destination','correlation','4','expiration','priority','true','reply-to','123456789']");
+    }
 
-	@Test
-	public void testFullLoadJson() throws Exception {
-		loadFile("classpath:/CreateJmsResponse_fullLoad.json");
-	}
+    @Test
+    public void testFullLoadJson() throws Exception {
+        loadFile("classpath:/CreateJmsResponse_fullLoad.json");
+    }
 }
