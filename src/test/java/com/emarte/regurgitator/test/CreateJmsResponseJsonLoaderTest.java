b3cb4b13098234bee05f0f9ec85e65efcdbef14d
talmeym <mtalmey@gmail.com>
2019-04-10 16:45:45 +0100

    added tests for multiple processors

--- src/test/java/com/emarte/regurgitator/test/CreateJmsResponseJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/CreateJmsResponseJsonLoaderTest.java
@@ -8,10 +8,11 @@ import com.emarte.regurgitator.extensions.mq.CreateJmsResponseJsonLoader;
 import org.junit.Test;
 
 import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+import static com.emarte.regurgitator.test.MqExtensionsLoaderTestExpectations.*;
 import static com.emarte.regurgitator.test.MqExtensionsLoaderTestExpectations.CreateJmsResponse;
+import static com.emarte.regurgitator.test.MqExtensionsLoaderTestExpectations.CreateJmsResponse_processor;
 
 public class CreateJmsResponseJsonLoaderTest extends JsonLoaderTest {
-
     public CreateJmsResponseJsonLoaderTest() {
         super(new CreateJmsResponseJsonLoader());
     }
@@ -19,6 +20,16 @@ public class CreateJmsResponseJsonLoaderTest extends JsonLoaderTest {
     @Test
     public void testThis() throws Exception {
         assertExpectation("classpath:/CreateJmsResponse.json", CreateJmsResponse);
+    }
+
+    @Test
+    public void testProcessor() throws Exception {
+        assertExpectation("classpath:/CreateJmsResponse_processor.json", CreateJmsResponse_processor);
+    }
+
+    @Test
+    public void testMutipleProcessor() throws Exception {
+        assertExpectation("classpath:/CreateJmsResponse_multipleProcessor.json", CreateJmsResponse_multipleProcessor);
     }
 
     @Test
